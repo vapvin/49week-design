35d3ba71cff71f6f85d2fd25b0aab92ce4a43ffe
chelsihoon <95158498+chelsihoon@users.noreply.github.com>
2022-01-11 16:21:14 +0900

    수정/변경ver.3.0
    git commit -m 수정/변경ver.3.0

--- design_detail.php
+++ design_detail.php
@@ -17,26 +17,34 @@
             <span></span>
         </div>
         <div class="wrap_page_list">
-            <a href="design_list.php" class="go_home">HOME</a>
             <nav class="list_page">
-                <a href="design_detail.php">A 시안 메인 페이지 1</a>
-                <a href="javascript:">A 시안 메인 페이지 2</a>
-                <a href="javascript:">A 시안 서브 페이지 1</a>
-                <a href="javascript:">A 시안 서브 페이지 2</a>
-                <a href="javascript:">A 시안 서브 페이지 3</a>
+                <div class="inner_page_list">
+                    <div class="list_main list">
+                        <h3>메인</h3>
+                        <a href="design_detail.php">A 시안 메인 페이지 1</a>
+                        <a href="javascript:">A 시안 메인 페이지 2</a>
+                    </div>
+                    <div class="list_sub list">
+                        <h3>서브</h3>
+                        <a href="javascript:">A 시안 서브 페이지 1</a>
+                        <a href="javascript:">A 시안 서브 페이지 2</a>
+                        <a href="javascript:">A 시안 서브 페이지 3</a>
+                    </div>
+                    <a href="design_list.php" class="go_home">첫 화면으로</a>
+                </div>
             </nav>
         </div>
     </div>
+    <h2 class="list_title">A 디자인 ver.2 (요청 사항 반영)</h2>
     <div class="wrap_title">
-      <h2 class="list_title">A 디자인 ver.2 (요청 사항 반영)</h2>
-      <div class="box_checks">
-          <input type="radio" name="checked" value="" id="final_decision_1">
-          <label for="final_decision_1"></label> 
-          <button>확정하기</button>
-      </div>
+        <div class="box_checks">
+            <input type="radio" name="checked" value="" id="final_decision_1">
+            <label for="final_decision_1"></label> 
+            <button>확정하기</button>
+        </div>
     </div>
   </header>
-  <img src="img/poonglim_index034.png" alt="">
+  <img src="img/poonglim_sample_1.jpg" alt="" class="body_img">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script>
       $(function(){
@@ -44,11 +52,12 @@
             $(this).toggleClass('active');
             if($(this).attr('data-click-state') == 1) {
                 $(this).attr('data-click-state', 0);
-                $('.list_page').slideUp(300,'linear');
+                $('.list_page').fadeOut(300);
             }else{
                 $(this).attr('data-click-state', 1);
-                $('.list_page').slideDown(300, 'swing');
+                $('.list_page').fadeIn(300);
             }
+            // slidedown은 자식에게 안먹힘.. 그래서 fadein으로 교체
         });
         $('.box_checks button').on('click', function(){
             let result = confirm("최종결정 하시겠습니까?");
@@ -56,7 +65,22 @@
                 alert("디자인 선택이 최종결정 완료 되었습니다.");
             }
         });
+        // let i = $('.inner_page_list .list div');
+        // function tet(e){
+        //     let index = e.target.dataset.index;
+        //     $('.body_img').css("src","img/poonglim_sample_"+i[index]+".jpg")
+        // }
+        // $('.inner_page_list .list div').click(function(e){
+            // e.preventDefault();
+            // tet(e);
+        // });
+        // i.forEach((item,index) => console.log(item, index));
+        // $('.inner_page_list .list div').each(function(index){
+        //     $(this).click
+        // })
       });
+
+      
   </script>
 </body>
 </html>
--- design_list.php
+++ design_list.php
@@ -10,7 +10,10 @@
 <body>
     <a href="#" class="btn_top">TOP</a>
     <header>
-        <h1 class="logo"><img src="img/49week_ci.png" alt="49week 로고"></h1>
+        <h1 class="logo">
+            <!-- <img src="img/49week_ci.png" alt="49week 로고"> -->
+            풍림철강
+        </h1>
         <nav class="gnb">
             <a href="javascript:"></a>
             <a href="javascript:"></a>
@@ -23,41 +26,57 @@
     </header>
     <section class="contents">
         <div class="cnt_1">
-            <div class="making_date">2022. 01. 05.</div>
             <div class="wrap_cnt">
                 <div class="wrap_title">
-                    <a href="design_detail.php">
-                        <h2 class="list_title">A 디자인 ver.2 (요청 사항 반영)</h2>
-                    </a>
                     <div class="box_checks">
+                        <a href="design_detail.php">
+                            <h2 class="list_title">A 디자인 ver.2 (요청 사항 반영)</h2>
+                        </a>
                         <input type="radio" name="checked" value="" id="final_decision_1">
                         <label for="final_decision_1"></label> 
                         <button>확정하기</button>
                     </div>
+                    <div class="making_date">2022.  01. 05.</div>
                 </div>
                 <div class="wrap_list">
                     <a href="design_detail.php" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
-                        <div class="box_img"><img src="img/poonglim_index034.png" alt=""></div>
+                        <div class="over_box">
+                            <!-- 확정 완료 시 아래 img tag display : block, p tag display : none 변경. 현재 img tag display : none  -->
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
+                        <div class="box_img"><img src="img/poonglim_sample_1.jpg" alt=""></div>
                         <h3 class="title_img">A 시안 메인 페이지 1</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">A 시안 메인 페이지 2</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
-                        <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
+                        <div class="box_img"><img src="img/poonglim_sample_2.jpg" alt=""></div>
                         <h3 class="title_img">A 시안 서브 페이지 1</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
-                        <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p> 
+                        </div>
+                        <div class="box_img"><img src="img/poonglim_sample_3.jpg" alt=""></div>
                         <h3 class="title_img">A 시안 서브 페이지 2</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">A 시안 서브 페이지 3</h3>
                     </a>
@@ -65,41 +84,56 @@
             </div> 
         </div> 
         <div class="cnt_2">
-            <div class="making_date">2022. 01. 04.</div>
             <div class="wrap_cnt">
                 <div class="wrap_title">
-                    <a href="javascript:">
-                        <h2 class="list_title">A 디자인</h2>
-                    </a>
                     <div class="box_checks">
+                        <a href="javascript:">
+                            <h2 class="list_title">A 디자인</h2>
+                        </a>
                         <input type="radio" name="checked" value="" id="final_decision_2">
                         <label for="final_decision_2"></label> 
                         <button>확정하기</button>
                     </div>
+                    <div class="making_date">2022. 01. 04.</div>
                 </div>
                 <div class="wrap_list">
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">A 시안 메인 페이지 1</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">A 시안 메인 페이지 2</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">A 시안 서브 페이지 1</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">A 시안 서브 페이지 2</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">A 시안 서브 페이지 3</h3>
                     </a>
@@ -107,41 +141,56 @@
             </div>
         </div>
         <div class="cnt_3">
-            <div class="making_date">2022. 01. 03.</div>
             <div class="wrap_cnt">
                 <div class="wrap_title">
-                    <a href="javascript:">
-                        <h2 class="list_title">B 디자인</h2>
-                    </a>
                     <div class="box_checks">
+                        <a href="javascript:">
+                            <h2 class="list_title">B 디자인</h2>
+                        </a>
                         <input type="radio" name="checked" value="" id="final_decision_3">
                         <label for="final_decision_3"></label> 
                         <button>확정하기</button>
                     </div>
+                    <div class="making_date">2022. 01. 03.</div>
                 </div>
                 <div class="wrap_list">
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">B 시안 메인 페이지 1</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">B 시안 메인 페이지 2</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">B 시안 서브 페이지 1</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">B 시안 서브 페이지 2</h3>
                     </a>
                     <a href="javascript:" class="inner_list">
-                        <div class="over_box"><p>+ 자세히 보기</p></div>
+                        <div class="over_box">
+                            <img src="img/icon_checked.png" alt="확정완료" class="confirmed">
+                            <p>+ 자세히 보기</p>
+                        </div>
                         <div class="box_img"><img src="https://via.placeholder.com/200x300" alt=""></div>
                         <h3 class="title_img">B 시안 서브 페이지 3</h3>
                     </a>
@@ -151,11 +200,13 @@
 </section>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script>
-        $('.box_checks button').on('click', function(){
-            let result = confirm("확정완료 하시겠습니까?");
-            if(result){
-                alert("디자인 선택이 최종 확정완료 되었습니다.");
-            }
+        $(function(){
+            $('.box_checks button').on('click', function(){
+                let result = confirm("확정완료 하시겠습니까?");
+                if(result){
+                    alert("디자인 선택이 최종 확정완료 되었습니다.");
+                }
+            });
         });
     </script>
 </body>
