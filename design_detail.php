ed5fd2fb6d54cee57b83a0a1f80b32ec34c52c7c
chelsihoon <95158498+chelsihoon@users.noreply.github.com>
2022-01-12 11:04:19 +0900

    수정/변경ver.3.1

--- design_detail.php
+++ design_detail.php
@@ -21,7 +21,7 @@
                 <div class="inner_page_list">
                     <div class="list_main list">
                         <h3>메인</h3>
-                        <a href="design_detail.php">A 시안 메인 페이지 1</a>
+                        <a href="design_detail.php" class="selected">A 시안 메인 페이지 1</a>
                         <a href="javascript:">A 시안 메인 페이지 2</a>
                     </div>
                     <div class="list_sub list">
@@ -30,18 +30,13 @@
                         <a href="javascript:">A 시안 서브 페이지 2</a>
                         <a href="javascript:">A 시안 서브 페이지 3</a>
                     </div>
-                    <a href="design_list.php" class="go_home">첫 화면으로</a>
                 </div>
             </nav>
         </div>
     </div>
-    <h2 class="list_title">A 디자인 ver.2 (요청 사항 반영)</h2>
     <div class="wrap_title">
-        <div class="box_checks">
-            <input type="radio" name="checked" value="" id="final_decision_1">
-            <label for="final_decision_1"></label> 
-            <button>확정하기</button>
-        </div>
+        <a href="design_list.php" class="go_home">뒤로가기</a>
+        <button class="btn_check">확정하기</button>
     </div>
   </header>
   <img src="img/poonglim_sample_1.jpg" alt="" class="body_img">
@@ -59,25 +54,12 @@
             }
             // slidedown은 자식에게 안먹힘.. 그래서 fadein으로 교체
         });
-        $('.box_checks button').on('click', function(){
+        $('.btn_check').on('click', function(){
             let result = confirm("최종결정 하시겠습니까?");
             if(result){
                 alert("디자인 선택이 최종결정 완료 되었습니다.");
             }
         });
-        // let i = $('.inner_page_list .list div');
-        // function tet(e){
-        //     let index = e.target.dataset.index;
-        //     $('.body_img').css("src","img/poonglim_sample_"+i[index]+".jpg")
-        // }
-        // $('.inner_page_list .list div').click(function(e){
-            // e.preventDefault();
-            // tet(e);
-        // });
-        // i.forEach((item,index) => console.log(item, index));
-        // $('.inner_page_list .list div').each(function(index){
-        //     $(this).click
-        // })
       });
 
       
